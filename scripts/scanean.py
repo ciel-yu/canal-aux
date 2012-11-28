885a9db80d1ff79ff49957e9b352b1fcf97a404b
Ciel <ciel-yu@users.noreply.github.com>
2012-11-28 16:20:11 +0000

    tidy scanean.py

--- scripts/scanean.py
+++ scripts/scanean.py
@@ -1,105 +1,44 @@
 # -*- coding: UTF-8 -*-
 
 
-from optparse import OptionParser
+from tia.barcode import InbarcodeOcr
+import argparse
 import os
 import re
 import sys
-import win32com.client
 
 
 def main():
-	parser = OptionParser( version="%prog 1.0", usage='Usage: %prog [options]' )
+	parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter )
 
 	parser.set_defaults( **{
 		'verbose': False,
 		'version': False
 	} )
 
-	parser.add_option( '-v', '--verbose', action='store_true' )
-	parser.add_option( '-V', dest='version', action='store_true' )
+	parser.add_argument( '-v', '--verbose', action='store_true' )
+	parser.add_argument( '-V', dest='version', action='store_true' )
 
-	opts, args = parser.parse_args()
+	opts = parser.parse_args()
 
-
-	ocr = win32com.client.Dispatch( "INBarcodeCOM.INBarcode" )
-
-	if not ocr:
-		print( "INBarcodeCOM not found" )
-		sys.exit();
-
-	registered = ocr.RegisterBarcodeDLL( "ciel", "EDACE8EB48EA4" )
+	ocr = InbarcodeOcr( "ciel", "EDACE8EB48EA4" )
 
 	if opts.version:
-		print( 'Registered:', registered )
-		print( 'INBarcode version:', ocr.GetBarcodeVersionInfo() )
+		print( 'Registered:', ocr.registered )
+		print( 'INBarcode version:', ocr.version )
 		sys.exit();
 
 	r = re.compile( r'#(\d{13}|\d{9}[0-9xX])|^-', re.I | re.U )
 
-	dirs = ( x for x in os.listdir( '.' ) if os.path.isdir( x ) and not r.findall( x ) )
 
-	ocr.DisableEAN8 = True
-	ocr.DisableCode39 = True
-	ocr.DisableCode128 = True
-	ocr.Disable2of5 = True
-	ocr.DisableRotation = True
-	ocr.UseIncreasedSensitivity = True
+	for root in ( x for x in os.listdir( '.' ) if os.path.isdir( x ) and not r.findall( x ) ):
 
-	ocr.UseFineSearch = True
-
-	if opts.verbose:
-		print( 'UseFineSearch' )
-
-	for dir in dirs:
-		fs = os.listdir( dir )
-		if '~.bmp' in fs:
-			fs = ['~.bmp']
-		files = list( x for x in ( dir + os.path.sep + x for x in fs ) if os.path.isfile( x ) )
-
-		if not files:
-			continue
-
-		ean = None
-
-		# scan filenames
-		for file in files:
-			if re.match( '^(\d{13}|\d{9}[0-9xX])$', os.path.splitext( os.path.basename( file ) )[0], re.UNICODE ):
-				ean = os.path.splitext( os.path.basename( file ) )[0]
-				break;
-
-		# scan image
-		if not ean:
-			if len( files ) > 10:
-				t = [None] * 10
-				t[::2] = files[:5:]
-				t[1::2] = files[:-6:-1]
-				files = t
-
-			for file in files:
-				print( 'checking:', file )
-				count = ocr.FindBarcodesFile( file )
-				codes = []
-				for i in range( count ):
-					codes.append( re.sub( r'\s+', '', ocr.GetBarcodeCode( i + 1 ) ) )
-
-				# 978 for ISBN13(EAN), 49/45 for JAN
-				for code in codes:
-					if re.match( '978', code ):
-						ean = code
-						break
-				if not ean:
-					for code in codes:
-						if re.match( '49|45', code ):
-							ean = code
-							break
-				if ean:
-					break
+		ean = ocr.search( root )
 
 		if ean:
 			print( 'got:', ean )
 			try:
-				os.renames( dir, '[#' + ean + ']' + dir )
+				os.renames( root, '[#' + ean + ']' + root )
 			except:
 				print( 'cannot rename:', dir )
 
